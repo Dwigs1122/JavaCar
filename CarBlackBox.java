b9c6194bc280e573ebdc504a182ebce2e8ca561b
dwigs <95131369+Dwigs1122@users.noreply.github.com>
2023-12-19 17:45:07 -0700

    Create CarBlackBox.java

---
+++ CarBlackBox.java
@@ -0,0 +1,37 @@
+Find what you need faster … Home is your new landing page and surfaces your most relevant files and folders
+package reference;
+// Car.java file
+
+// public class
+public class CarBlackBox {
+    // public variable
+    public int tireCount;
+
+    // public method
+    public void display() {
+        System.out.println(" ");
+        System.out.println("                                 _________");
+        System.out.println("                          _.--\"\"'-----,   `\"--.._");
+        System.out.println("                       .-''   _/_      ; .'\"----,`-,");
+        System.out.println("                     .'      :___:     ; :      ;;`.`.");
+        System.out.println("                    .      _.- _.-    .' :      ::  `..");
+        System.out.println("                 __;..----------------' :: ___  ::   ;;");
+        System.out.println("            .--\"\". '           ___.....`:=(___)-' :--'`.");
+        System.out.println("          .'   .'         .--''__       :       ==:    ;");
+        System.out.println("      .--/    /        .'.''     ``-,   :         :   '`-.");
+        System.out.println("   .\"', :    /       .'-`\\\\       .--.\\ :         :  ,   _\\");
+        System.out.println("  ;   ; |   ;       /:'  ;;      /__  \\\\:         :  :  /_\\\\");
+        System.out.println("  |\\_/  |   |      / \\__//      /\"--\\\\ \\:         :  : ;|`\\|    ");
+        System.out.println("  : \"  /\\__/\\____//   \"\"\"      /     \\\\ :         :  : :|'||");
+        System.out.println("[\"\"\"\"\"\"\"\"\"--------........._  /      || ;      __.:--' :|//|");
+        System.out.println(" \"------....______         ].'|      // |--\"\"\"'__...-'`\\ \\//");
+        System.out.println("   `| WVE230L |__;_...--'\": :  \\    //  |---\"\"\"      \\__\\_/");
+        System.out.println("     \"\"\"\"\"\"\"\"\"'            \\ \\  \\_.//  /");
+        System.out.println("       `---'                \\ \\_     _'");
+        System.out.println("                             `--`---'  dp");
+        System.out.println("");
+        
+        
+    }
+
+}
