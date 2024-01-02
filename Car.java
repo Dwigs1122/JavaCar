3c9a876042f76c6a87864d502c5b6499721dd2c2
dwigs <95131369+Dwigs1122@users.noreply.github.com>
2024-01-01 21:30:54 -0700

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,13 @@
+// Car.java file
+// public class
+public class Car {
+    // public variable
+    public int tireCount;
+
+    // public method
+    public void display() {
+        System.out.println("I am a Car.");
+        System.out.println("I have " + tireCount + " tires.");
+    }
+
+}
